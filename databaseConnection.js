37733a71ef7368c96685de8ab1eee9770ff4cfa1
Che-Yi Kung <kjohnathan.engi@gmail.com>
2021-02-17 21:12:59 -0800

    dbconfig changed

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,9 +5,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
+	user: "bdf70769937f85",
+	password: "8b70283f",
+	database: "heroku_058fe8747242c32",
 	multipleStatements: false
 };
 
